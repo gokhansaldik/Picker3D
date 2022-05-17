b5735416f67397a4a32ccb0509be4ed9c5100852
gokhansaldik <gokhansaldik@gmail.com>
2022-05-17 18:40:24 +0300

    fog added, finish plane updated, gate particle added

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,14 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Door : MonoBehaviour
 {
+
+    public GameObject Particle;
+    public ParticleSystem ParticleSpawner;
     public bool IsOpen = false;
     public float RotationSpeed = 1f;
     private float _rotationTimer = 1.4f;
     public Transform DoorLeft;
     public Transform DoorRight;
+
+
+   
     private void FixedUpdate()
     {
         if (IsOpen)
@@ -18,6 +25,9 @@ public class Door : MonoBehaviour
                 _rotationTimer -= Time.fixedDeltaTime;
                 DoorLeft.transform.Translate(-RotationSpeed, 0, 0);
                 DoorRight.transform.Translate(RotationSpeed, 0, 0);
+                Particle.SetActive(true);
+                ParticleSpawner.Play();
+
             }
             else
             {
@@ -25,4 +35,5 @@ public class Door : MonoBehaviour
             }
         }
     }
+    
 }
